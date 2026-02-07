56b77d9348d09cb75c0a89896f67ec4584d99fb7
Mustafa Abou El-Hajj <maboualhajj@gmail.com>
2026-02-07 11:44:08 +0200

    main

--- connection.php
+++ connection.php
@@ -1,12 +1,23 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $dbHost = 'localhost';
 $dbUsername = 'root';
 $dbPassword = '';
 $dbName = 'menu';
 
-$conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+// Set connection timeout to 5 seconds
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+    $conn->options(MYSQLI_OPT_CONNECT_TIMEOUT, 5);
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    die("Database connection error: " . $e->getMessage() . "<br>Make sure MySQL is running in XAMPP.");
 }
 ?>
